1d3c074c1586b82bcb366cd2a6ffd820910b785b
Erin Aurélie <erinaurelieessama@gmail.com>
2025-05-15 21:24:37 +0000

    Updated Porject structure by implementing Separation of concerns

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -9,8 +9,22 @@ const userRouter = Router();
 userRouter.get('/', authorize, adminMiddleware, getUsers);
 // get a user :: dynamic route
 userRouter.get('/:id', authorize, getUser); // authorize before getting the details
-userRouter.post('/', createUser);
+userRouter.post('/', authorize, createUser);
 userRouter.put('/:id', authorize, updateUser);
 userRouter.delete('/:id', authorize, deleteUser);
 
 export default userRouter;
+
+/*
+    Basic Switch (Single Active User at a Time)
+    When switching accounts:
+
+    Logout current user (delete JWT).
+
+    Redirect to login.
+
+    Log in with another user.
+
+    Store new JWT.
+
+*/
